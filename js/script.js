a8a051c86e3da777a84171c618a35007d9ed0ac7
ushastoe <40743392+krolchonok@users.noreply.github.com>
2023-08-28 23:00:50 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function copychrome() {
+    var text = "chrome://extensions/";
+    const input = document.createElement('textarea');
+    input.value = text;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+  }
